bdbdca6350ad2de1b15be602ff462000f3d7570e
RajkumarMandhniya <58131106+RajkumarMandhniya@users.noreply.github.com>
2021-06-30 15:39:54 +0530

    commit

--- spring-cloud-configuration-server/src/main/java/com/example/demo/SpringCloudConfigurationServerApplication.java
+++ spring-cloud-configuration-server/src/main/java/com/example/demo/SpringCloudConfigurationServerApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer //this micro will run as a spring cloud server everything already downloaded and will work as server
 public class SpringCloudConfigurationServerApplication {
 
 	public static void main(String[] args) {
