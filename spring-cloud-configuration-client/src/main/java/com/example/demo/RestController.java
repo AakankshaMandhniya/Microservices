8686782ef19504da8107dce19897c4b8ea2830b2
RajkumarMandhniya <58131106+RajkumarMandhniya@users.noreply.github.com>
2021-06-30 16:16:19 +0530

    12

--- spring-cloud-configuration-client/src/main/java/com/example/demo/RestController.java
+++ spring-cloud-configuration-client/src/main/java/com/example/demo/RestController.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-
-	String message = "Welcome to Fis";
-	String name = "Aaks";
-	@GetMapping("/greetings")
-	public String greetings()
+	
+	@Value("${greeting.message}")
+	 String message;
+	@Value("${greeting.name}")
+	String name;
+	/*
+	 * String message = "Welcome to Fis"; String name = "Aaks";
+	 */
+	@GetMapping("/greeting")
+	public String greeting()
 	{
 		return message +" ----->" + name;
 		
